a8630ae5c0c23edb48a7f2baf631d774f8e51c56
DvD811 <davide.gallizioli@gmail.com>
2019-07-17 16:12:17 +0200

    a

---
+++ main.js
@@ -0,0 +1,17 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.Stampare a schermo attraverso il for in tutte le proprietà.
+var bio = {
+    "nome": "Davide",
+    "cognome": "Gallizioli",
+    "et&agrave": "27"
+};
+for (var bioprint in bio) {
+    console.log(bio[bioprint])
+};
+// - Creare un array di oggetti di studenti.Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+var biostudenti = [
+        { "nome": "Davide", "cognome": "Gallizioli", "et&agrave": "27" } { "nome": "Davide", "cognome": "Gallizioli", "et&agrave": "27" } { "nome": "Davide", "cognome": "Gallizioli", "et&agrave": "27" }
+    ]
+    //     - Dare la possibilità all’utente attraverso 3 prompt di
+    // aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+// * NOTE=* quindi repo con  cartella e  tre commentoni nello stesso JS che definisce i tre macrostep
