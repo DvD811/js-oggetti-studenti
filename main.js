3bb77212fc6b54e969690307bd292cead6dad1ff
DvD811 <davide.gallizioli@gmail.com>
2019-07-19 08:42:54 +0200

    upd

--- main.js
+++ main.js
@@ -2,18 +2,18 @@
 var bio = {
     "nome": "Davide",
     "cognome": "Gallizioli",
-    "et&agrave": "27"
+    "eta": "27"
 };
 for (var bioprint in bio) {
     console.log(bio[bioprint])
 };
 // - Creare un array di oggetti di studenti.Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 var biostudenti = [
-    { "nome": "jojo", "cognome": "Gigio", "et&agrave": "67" },
-    { "nome": "Daine", "cognome": "Gizioli", "et&agrave": "65" },
-    { "nome": "Donato", "cognome": "Galli", "et&agrave": "21" },
-    { "nome": "Bart", "cognome": "Rossi", "et&agrave": "24" },
-    { "nome": "Johnny", "cognome": "Verdi", "et&agrave": "37" },
+    { "nome": "Daine", "cognome": "Gizioli", "eta": "65" },
+    { "nome": "jojo", "cognome": "Gigio", "eta": "67" },
+    { "nome": "Donato", "cognome": "Galli", "eta": "21" },
+    { "nome": "Bart", "cognome": "Rossi", "eta": "24" },
+    { "nome": "Johnny", "cognome": "Verdi", "eta": "37" },
 ]
 for (var i = 0; i < biostudenti.length; i++) {
 
@@ -23,8 +23,12 @@ for (var i = 0; i < biostudenti.length; i++) {
 // aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 var insertname = prompt("Inserisci qui il tuo nome:");
 var insertsurname = prompt("Inserisci qui il tuo cognome:");
-var insertage = prompt("Inserisci qui la tua et&agrave:");
-biostudenti.push({ "nome": insertname, "cognome": insertsurname, "et&agrave": insertage });
-console.log(biostudenti);
-document.getElementById("result").innerHTML = bio;
+var insertage = prompt("Inserisci qui la tua eta:");
+biostudenti.push({ "nome": insertname, "cognome": insertsurname, "eta": insertage });
+var printd = document.getElementById("result").innerHTML;
+for (var stud in biostudenti) {
+    // document.getElementById("result").innerHTML = biostudenti[stud];
+    console.log(biostudenti[stud]);
+}
+
 // * NOTE=* quindi repo con  cartella e  tre commentoni nello stesso JS che definisce i tre macrostep
